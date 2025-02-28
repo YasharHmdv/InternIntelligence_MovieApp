ede7f8d240340d2d193f415b191fb9cee247cec2
Yashar Hamidov <yashar.gamidov@gmail.com>
2025-02-28 17:26:09 +0400

    modified

--- src/main/java/com/example/movieapp/controller/MovieController.java
+++ src/main/java/com/example/movieapp/controller/MovieController.java
@@ -3,18 +3,15 @@ package com.example.movieapp.controller;
 import com.example.movieapp.entity.Genres;
 import com.example.movieapp.entity.Languages;
 import com.example.movieapp.entity.Movie;
+import com.example.movieapp.entity.Review;
 import com.example.movieapp.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.springframework.boot.Banner.Mode.LOG;
 
 @RestController
 @RequiredArgsConstructor
@@ -40,5 +37,30 @@ public class MovieController {
                 .collect(Collectors.toList());
         LOG.debug(": " + ratedMovies.size());
         return ratedMovies;
+    }
+    @RequestMapping(value = "/{movieId}", method = RequestMethod.GET)
+    public List<Movie> getMovieInfo(@PathVariable("movieId") Integer movieId) {
+        return movieService.getMovieInfo(movieId);
+    }
+
+    @RequestMapping(value = "/review", method = RequestMethod.POST)
+    public List<Movie> addMovieReview(@RequestBody Review reviews) {
+        LOG.info("Add Movie Reviews...");
+        movieService.addReview(reviews);
+
+        return movieService.getMovieInfo(reviews.getMovieId());
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Movie> getAllMovies() {
+        LOG.info("Fetch all the Movies...");
+        return movieService.getAllMovies();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public List<Movie> addMovie(@RequestBody Movie movie) {
+        LOG.info("Add a Movie...");
+        movieService.addMovie(movie);
+        return movieService.getAllMovies();
     }
 }
